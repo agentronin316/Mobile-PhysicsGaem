604287c7a55d4a93c51ea234b0c7b2886569514c
agentronin316 <agentronin316@gmail.com>
2016-04-14 19:10:14 -0500

    In-class work completed
    Yay.

--- PhysicsGaem/Assets/Scripts/ScriptCanon.cs
+++ PhysicsGaem/Assets/Scripts/ScriptCanon.cs
@@ -1,22 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ScriptCanon : MonoBehaviour {
 
     public GameObject cannonBall;
+    public Image powerBar;
+    public Image rotationIndicator;
+    public Image ammoMask;
+
+    public int numShots = 5;
+    int shotsRemaining;
+
+    [Header("Power Settings")]
     public float power = 30f;
     public float maxPower = 40f;
     public float minPower = 20f;
-    public float cannonRotation = 49f;
-    public float minCannonRotation = 30f;
-    public float maxCannonRotation = 60f;
 
+    [Header("Rotation Settings")]
+    public float cannonRotation = 47f; //Initial cannon angle
+    public float minCannonRotation = 30f; //Minimum cannon angle
+    public float maxCannonRotation = 60f; //Maximum cannon angle
+    public float rotationSpeed = 10f; //Cannon rotation per click
+
+    float inversePowerRange;
     float degToRad = 0.0174533f;
 
+    int numStars;
+    int starsCollected = 0;
+
     // Use this for initialization
-    void Start () {
-	
-	}
+    void Start ()
+    {
+        inversePowerRange = 1 / (maxPower - minPower);
+        powerBar.fillAmount = (power - minPower) * inversePowerRange;
+        rotationIndicator.fillAmount = (cannonRotation / 360);
+        shotsRemaining = numShots;
+        ammoMask.fillAmount = (float)shotsRemaining / numShots;
+        numStars = GameObject.FindGameObjectsWithTag("Star").Length;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -26,6 +48,7 @@ public class ScriptCanon : MonoBehaviour {
             if (power < maxPower)
             {
                 power += 1;
+                powerBar.fillAmount = (power - minPower) * inversePowerRange;
             }
         }
         else if (Input.GetKeyDown(KeyCode.S))
@@ -33,33 +56,60 @@ public class ScriptCanon : MonoBehaviour {
             if (power > minPower)
             {
                 power -= 1;
+                powerBar.fillAmount = (power - minPower) * inversePowerRange;
             }
         }
+
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (cannonRotation < maxCannonRotation)
             {
-                cannonRotation += 1;
-                transform.Rotate(Vector3.forward);
+                cannonRotation += rotationSpeed;
+                transform.Rotate(Vector3.forward * rotationSpeed);
+                rotationIndicator.fillAmount = (cannonRotation / 360);
             }
         }
         else if(Input.GetKeyDown(KeyCode.D))
         {
             if (cannonRotation > minCannonRotation)
             {
-                cannonRotation -= 1;
-                transform.Rotate(Vector3.back);
+                cannonRotation -= rotationSpeed;
+                transform.Rotate(Vector3.back * rotationSpeed);
+                rotationIndicator.fillAmount = (cannonRotation / 360);
             }
         }
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            CannonBalls();
+            if (shotsRemaining > 0)
+            {
+                CannonBalls();
+                shotsRemaining--;
+                ammoMask.fillAmount = (float)shotsRemaining / numShots;
+                
+            }
+            else
+            {
+                Debug.Log("Game Over");
+            }
         }
 	}
+
+    public void DestroyStar(GameObject star)
+    {
+        starsCollected++;
+        Destroy(star);
+        if (starsCollected >= numStars)
+        {
+            Debug.Log("Winning!");
+        }
+    }
+
 
     void CannonBalls()
     {
         GameObject cannonBallInstance = (GameObject)Instantiate(cannonBall, transform.position, Quaternion.identity);
-        cannonBallInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(power * Mathf.Cos(cannonRotation * degToRad), power * Mathf.Sin(cannonRotation * degToRad));
+        cannonBallInstance.GetComponent<Rigidbody2D>().velocity =
+            new Vector2(power * Mathf.Cos(cannonRotation * degToRad), power * Mathf.Sin(cannonRotation * degToRad));
     }
 }
---
+++ PhysicsGaem/Assets/Scripts/ScriptStar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptStar : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.tag == "GameController")
+        {
+            GameObject.Find("Cannon").GetComponent<ScriptCanon>().DestroyStar(gameObject);
+        }
+    }
+}
